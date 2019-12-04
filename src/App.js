ffbcc500f41d324a9929de4530a0d064428c66a6
RomaoFelipe <romaofeilpe@outlook.com.br>
2019-12-04 01:08:39 -0300

    Added material-ui and start develop the home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import TextCounter from "./components/text_counter";
-import TextReverse from "./components/text_reverse";
-import AlphabeticOrder from "./components/alphabetic_order";
-import TextUpperLower from "./components/text_upperlower";
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
-        <TextUpperLower></TextUpperLower>
+        <Home></Home>
     </div>
   );
 }
